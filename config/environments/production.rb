f91be13ebdc36d3f756d1b6bbb25659b194392bb
Joseph Ridgway <joseph@comsoft155.com>
2011-10-03 10:16:31 -0400

    Added mail settings required for cedar stack

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,15 @@ Soepi::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   
   config.action_mailer.default_url_options = { :host => 'soepi.org' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
   # Enable threaded mode
   # config.threadsafe!
