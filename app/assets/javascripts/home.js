865e505c9f05498113ea287ad5a6087be8f94c52
Joseph Ridgway <joseph@comsoft155.com>
2011-11-01 09:40:24 -0400

    Fixes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -16,15 +16,17 @@ function init_slider() {
 }
 
 function init_auto_load_notifications() {
-  $('.bp.home #footer').appear(function() {
-    notifications_page += 1;
-    $('#notifications').append('<li class="ajax-loader-container"><img src="/assets/ajax-loader.gif" class="ajax-loader" /></li>');
-    $.get('/notifications?page=' + notifications_page, function(data) {
-      $('#notifications .ajax-loader-container').remove();
-      $('#notifications').append(data);
-      if(data != '') {
-        init_auto_load_notifications();
-      }
+  if($('.home #notifications').length > 0) {
+    $('#footer').appear(function() {
+      notifications_page += 1;
+      $('#notifications').append('<li class="ajax-loader-container"><img src="/assets/ajax-loader.gif" class="ajax-loader" /></li>');
+      $.get('/notifications?page=' + notifications_page, function(data) {
+        $('#notifications .ajax-loader-container').remove();
+        $('#notifications').append(data);
+        if(data != '') {
+          init_auto_load_notifications();
+        }
+      });
     });
-  });
+  }
 }
