3d4336bbd29b93bea3743a956f3a23fd290b0919
Joseph Ridgway <joseph@comsoft155.com>
2011-12-02 17:35:05 -0500

    qf

--- app/models/report.rb
+++ app/models/report.rb
@@ -56,7 +56,8 @@ class Report < ActiveRecord::Base
       File.open(local_script_name, 'w') {|f| f.write(report.code + "\n")}
       ec2_instance.scp_upload(local_script_name, remote_script_name)
       local_script_name.delete
-      results = ec2_instance.ssh("R < #{remote_script_name} --vanilla")[0].stdout.strip.slice(s.index('>')..-1)
+      results = ec2_instance.ssh("R < #{remote_script_name} --vanilla")[0].stdout.strip
+      results = results.slice(results.index('>')..-1) if results.index('>').to_i > 0
       report.update_attribute :results, results
       ec2_instance.ssh("convert -density 300 -resize 1000x1000\> -quality 100 Rplots.pdf Rplots.png")
       (0..1000).each do |i|
