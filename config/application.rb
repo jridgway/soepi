d779ef48f60543169c6c6051ed8b7e747140d6ce
Joseph Ridgway <joseph@comsoft155.com>
2012-01-18 15:31:13 -0500

    Added participant_response to filter_parameters

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module Soepi
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password, :password_confirmation, :pin]
+    config.filter_parameters += [:password, :password_confirmation, :pin, :participant_response]
 
     # Enable the asset pipeline
     config.assets.enabled = true
