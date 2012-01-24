acb9ac6bab0ce43221aa05135edf568144787f94
Joseph Ridgway <joseph@comsoft155.com>
2012-01-24 14:53:17 -0500

    More caching

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   layout 'two_column_wide'
+  caches_action :show,
+    :cache_path => Proc.new {|controller| controller.params}, 
+    :expires_in => 15.minutes
 
   def show
     if @page = Page.find_by_slug_and_state(params[:id], 'published')
