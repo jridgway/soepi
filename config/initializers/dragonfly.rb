4f8076f0da831635d6797f9b96468e27aa5a1071
Joseph Ridgway <joseph@comsoft155.com>
2012-02-29 12:34:27 -0500

    Configured dragonfly for s3

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -3,7 +3,6 @@ app = Dragonfly[:images]
 
 app.configure_with(:imagemagick)
 app.configure_with(:rails)
-app.configure_with(:heroku, 'soepi') if Rails.env.production?
 
 app.define_macro(ActiveRecord::Base, :image_accessor)
 app.define_macro(ActiveRecord::Base, :file_accessor)
@@ -11,4 +10,11 @@ app.define_macro(ActiveRecord::Base, :file_accessor)
 app.configure do |c|
   c.protect_from_dos_attacks = true
   c.secret = 'dfd123ghjhjkiouom34ojoijwe98dfngovsm58lty58sgg32543vt442kjf5h2k3jvlni3485c834yl85nv23485ni34nxpop24m'
+  if Rails.env.production?
+    c.datastore = Dragonfly::DataStorage::S3DataStore.new(
+      :bucket_name => ENV['S3_BUCKET'],
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    )
+  end
 end
