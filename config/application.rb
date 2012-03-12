224489e8699763f6077312e50ef412650de796f9
Joseph Ridgway <joseph@comsoft155.com>
2012-03-12 14:44:07 -0400

    Config changes for assets

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,9 @@ require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module Soepi
@@ -63,7 +63,7 @@ module Soepi
     config.assets.version = '1.0'
     config.middleware.insert 0, 'Dragonfly::Middleware', :images
     
-    #config.assets.precompile += ['screen.css', 'print.css']
+    config.assets.precompile += ['screen.css', 'print.css']
     config.assets.initialize_on_precompile = false
   end
 end
