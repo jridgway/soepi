b1f1b0eb31f3dab06c9d077c06af659b7eb4f839
Joseph Ridgway <joseph@comsoft155.com>
2012-03-13 14:04:38 -0400

    Added custom page caching

---
+++ app/decorators/controllers/refinery/page_sweeper_decorator.rb
@@ -0,0 +1,7 @@
+Refinery::PageSweeperController.class_eval do 
+  protected
+  
+    def expire_cache
+      Rails.cache.write :pages_cache_expirary_key, rand.to_s[2..-1]
+    end
+end
--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -1,3 +1,12 @@
 Refinery::PagesController.class_eval do 
   layout Proc.new { |controller| controller.request.xhr? ? 'ajax' : 'one_column' }
+  
+  caches_action :home, :show, 
+    :cache_path => Proc.new {|controller| cache_expirary_key(controller.params)}
+    
+  protected
+    
+    def cache_expirary_key(params)
+      params.merge :cache_expirary_key => Rails.cache.read(:pages_cache_expirary_key)
+    end
 end
