1c3b0efc6bc804e661d60bf978abd93c77b4b9d7
Joseph Ridgway <joseph@comsoft155.com>
2012-04-30 15:19:42 -0400

    Caching fix

--- app/sweepers/collaborators_sweeper.rb
+++ app/sweepers/collaborators_sweeper.rb
@@ -8,7 +8,7 @@ class CollaboratorsSweeper < ActionController::Caching::Sweeper
     end
   end
 
-  def after_destroy(survey)
+  def after_destroy(collaborator)
     case collaborator.collaborable_type.downcase
       when 'survey' then
         expire_action survey_questions_url(:survey_id => collaborator)
