c8f751754c1cf0cdb834bee9ec7304b16055b238
Joseph Ridgway <joseph@comsoft155.com>
2012-04-30 15:12:49 -0400

    Caching fix

--- app/sweepers/collaborators_sweeper.rb
+++ app/sweepers/collaborators_sweeper.rb
@@ -4,14 +4,14 @@ class CollaboratorsSweeper < ActionController::Caching::Sweeper
   def after_save(collaborator)
     case collaborator.collaborable_type.downcase
       when 'survey' then
-        expire_action survey_questions_url(:survey_id => collaborator.collaborable_id)
+        expire_action survey_questions_url(:survey_id => collaborator)
     end
   end
 
   def after_destroy(survey)
     case collaborator.collaborable_type.downcase
       when 'survey' then
-        expire_action survey_questions_url(:survey_id => collaborator.collaborable_id)
+        expire_action survey_questions_url(:survey_id => collaborator)
     end
   end
 end
--- app/sweepers/surveys_sweeper.rb
+++ app/sweepers/surveys_sweeper.rb
@@ -3,11 +3,11 @@ class SurveysSweeper < ActionController::Caching::Sweeper
 
   def after_save(survey)
     Rails.cache.write :surveys_cache_expirary_key, rand.to_s[2..-1]
-    expire_action survey_questions_url(:survey_id => survey.id)
+    expire_action survey_questions_url(:survey_id => survey)
   end
 
   def after_destroy(survey)
     Rails.cache.write :surveys_cache_expirary_key, rand.to_s[2..-1]
-    expire_action survey_questions_url(:survey_id => survey.id)
+    expire_action survey_questions_url(:survey_id => survey)
   end
 end
